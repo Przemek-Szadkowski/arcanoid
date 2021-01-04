99fa41c41ff0eea515a09104aaa4b83b06e0d8a1
Przemek-Szadkowski <przemoszadkowski@o2.pl>
2021-01-04 16:38:04 +0100

    Add stick functions to ball and combine them with paddle3 functions

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+MOVE_AMMOUNT = 20
 
 class Ball(Turtle):
     def __init__(self):
@@ -7,4 +8,12 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.goto(0, -310)
+        self.goto(0, -300)
+
+    def stick_to_paddle_move_left(self):
+        if self.xcor() > -325 and self.ycor() == -310:
+            self.backward(MOVE_AMMOUNT)
+
+    def stick_to_paddle_move_right(self):
+        if self.xcor() < 325 and self.ycor() == -310:
+            self.forward(MOVE_AMMOUNT)
--- main.py
+++ main.py
@@ -11,9 +11,20 @@ screen.tracer(0)
 paddle = Paddle()
 ball = Ball()
 
+
+def combine_two_functions_with_left_key():
+    paddle.move_left()
+    ball.stick_to_paddle_move_left()
+
+
+def combine_two_functions_with_right_key():
+    paddle.move_right()
+    ball.stick_to_paddle_move_right()
+
+
 screen.listen()
-screen.onkey(paddle.move_left, "Left")
-screen.onkey(paddle.move_right, "Right")
+screen.onkey(combine_two_functions_with_left_key, "Left")
+screen.onkey(combine_two_functions_with_right_key, "Right")
 
 game_is_on = True
 
